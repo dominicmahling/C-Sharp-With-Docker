c270e6dab9656d3a060c6b2cff787573edd51149
d0s1 <dominic1020@web.de>
2026-01-06 18:30:05 +0100

    redid port

--- CSharpWithDocker/Program.cs
+++ CSharpWithDocker/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "8060";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 app.MapGet("/", () => "Hello World!");
 
